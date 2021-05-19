d12fdfeb40fef50c94e06f637b5ce28b8ebaf243
Vlad Ventura <vladventura@outlook.es>
2021-05-19 10:59:04 -0400

    Moing the errors type to the types file

--- src/common/types.d.ts
+++ src/common/types.d.ts
@@ -1,6 +1,13 @@
 export type FormType = {
-    username: string,
-    password: string,
-    email?: string,
-    confirmPassword?: string
-}
+  username: string;
+  password: string;
+  email?: string;
+  confirmPassword?: string;
+};
+
+export type FormErrorsType = {
+  username: string | null;
+  email: string | null;
+  password: string | null;
+  confirmPassword: string | null;
+};
