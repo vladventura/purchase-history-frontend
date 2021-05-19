2dcc01e9dc05fa12af33bb0ebef7850d70e0bda1
Vlad Ventura <vladventura@outlook.es>
2021-05-19 12:50:42 -0400

    Adding a small change to the css choice for now, maybe later on I'll revert this. Also started scaffolding the home page as well

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,26 @@
-import { useState, useEffect, useContext } from "react";
+import { useContext } from "react";
 import { Redirect } from "react-router-dom";
+import { Container } from "semantic-ui-react";
 import { AuthContext } from "../context/auth";
-
+import "./Home.css";
 const Home = () => {
   const { user } = useContext(AuthContext);
   const redirect = <Redirect to="/login" />;
-  const home = <div>Home Page</div>;
+
+  const ProfileCard = (
+    <div className="ui segment grid profile-card">Profile Card</div>
+  );
+
+  const ItemsDisplay = (
+    <div className="ui segment grid items-display">Items Display</div>
+  );
+
+  const home = (
+    <Container className="middle grid column">
+      <div className="ui container row">{ProfileCard}</div>
+      <div className="ui container row">{ItemsDisplay}</div>
+    </Container>
+  );
   return user ? home : redirect;
 };
 
