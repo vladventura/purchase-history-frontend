7ee9c58a1e6daa317330205faa6af3d9dea143ef
Vladimir Ventura <vladventura@outlook.es>
2021-05-24 10:29:24 -0400

    Added safeguards for the numeric inputs from the add item page

--- src/utils/hooks.ts
+++ src/utils/hooks.ts
@@ -1,12 +1,31 @@
 import React, { useState } from "react"
-import { FormType } from "../common/types";
+import { AuthFormType, ItemFormType } from "../common/types";
 
-function OnForm(callback: Function, initState: FormType) {
+function OnForm(callback: Function, initState: AuthFormType | ItemFormType) {
     const [values, setValues] = useState(initState);
     const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        setValues({ ...values, [e.target.name]: e.target.value });
+        // Naive attempt to solve for numeric input
+        let val: string | number = "";
+        // Attempt 1: Failed because parse operations don't throw an exception
+        // try {
+        //     val = parseFloat(e.target.value);
+        // } catch (_) {
+        //     val = e.target.value;
+        // }
+
+        // Attempt 2: Works, but hard to scale with. A better option would be to add a FormType and
+        // return our stuff depending on the type
+        if (e.target.name === "price" || e.target.name === "cost") {
+            val = parseFloat(e.target.value);
+            if (isNaN(val)) {
+                val = 0.0
+            }
+        } else val = e.target.value;
+
+        setValues({ ...values, [e.target.name]: val });
     };
     const onSubmit = (e: React.FormEvent) => {
+        console.log(values)
         e.preventDefault();
         callback();
     };
