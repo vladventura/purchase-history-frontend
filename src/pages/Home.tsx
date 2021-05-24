45354fa437f624094890e788948f4122c7a4badc
Vladimir Ventura <vladventura@outlook.es>
2021-05-24 12:24:09 -0400

    Implementing the query over here

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,47 +5,20 @@ import { AuthContext } from "../context/auth";
 import { Item } from "../graphql/schemas";
 import { ProfileBanner } from "../components/ProfileBanner";
 import { ItemsDisplay } from "../components/ItemsDisplay";
+import { useQuery } from "@apollo/client";
+import { GetItemsQuery, GET_ITEMS_QUERY } from "../graphql/queries";
 const Home = () => {
   const { user } = useContext(AuthContext);
   const redirect = <Redirect to="/login" />;
-
-  const items: Array<Item> = [
-    {
-      name: "Item name",
-      cost: 9.95,
-      price: 15.99,
-      id: "buSGbeouaob",
-    },
-    {
-      name: "Item name",
-      cost: 9.95,
-      price: 15.99,
-      id: "oa9brh",
-    },
-    {
-      name: "Item name",
-      cost: 9.95,
-      price: 15.99,
-      id: "rurewv",
-    },
-    {
-      name: "Item name",
-      cost: 9.95,
-      price: 15.99,
-      id: "egwiub",
-    },
-    {
-      name: "Item name",
-      cost: 9.95,
-      price: 15.99,
-      id: "oeirhn",
-    },
-  ];
+  const { data, loading } = useQuery(GET_ITEMS_QUERY);
 
   const home = (
     <Container fluid>
       <ProfileBanner user={user} />
-      <ItemsDisplay items={items} loading={false} />
+      <ItemsDisplay
+        items={(data as GetItemsQuery)?.getItems}
+        loading={loading}
+      />
     </Container>
   );
   return user ? home : redirect;
