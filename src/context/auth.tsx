0185eaa2d82402485fcdd5d10e132bd35079e3ef
Vladimir Ventura <vladventura@outlook.es>
2021-05-24 20:28:46 -0400

    I was not aware, but line 35 was miswritten, which is probably why typescript was complaining when I tried to cast the end to an empty object as UserProfile. Also, adding and setting up the profile update as well. I'm doing the profile update here as well for the other cases because might as well do it now. The most tricky one will naturally be the update case too.

--- src/context/auth.tsx
+++ src/context/auth.tsx
@@ -1,7 +1,7 @@
 import { createContext, useReducer } from "react";
 import jwtDecode from "jwt-decode";
 import { JWT_TOKEN_KEY } from "../constants";
-import { User } from "../graphql/schemas";
+import { Item, User, UserProfile } from "../graphql/schemas";
 import { UserMutation } from "../graphql/mutations";
 
 // This is the expected result from jwtDecode
@@ -12,6 +12,7 @@ interface State {
   user: Jwtitem | null;
   login: (userData: UserMutation) => void;
   logout: () => void;
+  addItem: (item: Item) => void;
 }
 
 // This is like a replacement of Redux
@@ -19,6 +20,7 @@ const initState: State = {
   user: null,
   login: (userData: UserMutation) => {},
   logout: () => {},
+  addItem: (item) => {},
 };
 
 if (localStorage.getItem(JWT_TOKEN_KEY)) {
@@ -29,7 +31,9 @@ if (localStorage.getItem(JWT_TOKEN_KEY)) {
     localStorage.removeItem("userProfile");
     localStorage.removeItem(JWT_TOKEN_KEY);
   } else {
-    const profile = JSON.parse(localStorage.getItem("userProfile" || "") ?? "");
+    const profile =
+      JSON.parse(localStorage.getItem("userProfile") || "") ??
+      ({} as UserProfile);
     initState.user = { ...decoded, profile };
   }
 }
@@ -38,6 +42,7 @@ const AuthContext = createContext({
   user: null,
   login: (userData: UserMutation) => {},
   logout: () => {},
+  addItem: (item) => {},
 } as State);
 
 function authReducer(state: State, action: { type: string; payload?: any }) {
@@ -46,6 +51,30 @@ function authReducer(state: State, action: { type: string; payload?: any }) {
       return { ...state, user: action.payload };
     case "LOGOUT":
       return { ...state, user: null };
+    case "ADD_ITEM":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          profile: { ...state?.user?.profile, ...action.payload },
+        },
+      };
+    case "REMOVE_ITEM":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          profile: { ...state?.user?.profile, ...action.payload },
+        },
+      };
+    case "EDIT_ITEM":
+      return {
+        ...state,
+        user: {
+          ...state.user,
+          profile: { ...state?.user?.profile, ...action.payload },
+        },
+      };
     default:
       return state;
   }
@@ -76,9 +105,27 @@ function AuthProvider(props: Object) {
       type: "LOGOUT",
     });
   };
+  const addItem = (item: Item) => {
+    const localStorageProfile =
+      JSON.parse(localStorage.getItem("userProfile") || "") ??
+      ({} as UserProfile);
+
+    const newProfile = {
+      totalPrice: localStorageProfile?.totalPrice + item.price,
+      totalCost: localStorageProfile?.totalCost + item.cost,
+      totalAddedItems: localStorageProfile?.totalAddedItems + 1,
+    };
+
+    localStorage.setItem("userProfile", JSON.stringify(newProfile));
+
+    dispatch({
+      type: "ADD_ITEM",
+      payload: newProfile,
+    });
+  };
   return (
     <AuthContext.Provider
-      value={{ user: state.user, login, logout }}
+      value={{ user: state.user, login, logout, addItem }}
       {...props}
     />
   );
