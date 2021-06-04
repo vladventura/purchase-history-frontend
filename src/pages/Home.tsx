12698a7f5a3611c4b5ea998cb22dda47a3fdfc7e
Vladimir Ventura <vladventura@outlook.es>
2021-06-04 01:47:04 -0400

    Using the items context to get the items array and send it to the items display, and also using useEffect because whenever the app adds an item, it modifies the cache

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useContext } from "react";
+import { useContext, useEffect } from "react";
 import { Redirect } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import { AuthContext } from "../context/auth";
@@ -7,13 +7,19 @@ import { ItemsDisplay } from "../components/ItemsDisplay";
 import { useQuery } from "@apollo/client";
 import { GetItemsQuery, GET_ITEMS_QUERY } from "../graphql/queries";
 import { UIProvider } from "../context/ui";
+import { ItemsContext } from "../context/items";
 const Home = () => {
   const { user } = useContext(AuthContext);
+  const { items, setItems } = useContext(ItemsContext);
+
+  const { data, loading } = useQuery(GET_ITEMS_QUERY, {});
+
+  useEffect(() => {
+    setItems((data as GetItemsQuery)?.getItems);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [(data as GetItemsQuery)?.getItems]);
+
   const redirect = <Redirect to="/login" />;
-  const { data, loading } = useQuery(GET_ITEMS_QUERY);
-
-  const items = (data as GetItemsQuery)?.getItems;
-
   const home = (
     <Container fluid>
       <UIProvider>
