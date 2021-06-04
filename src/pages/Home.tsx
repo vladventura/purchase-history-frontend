dfb5604bdf0ffbbc56cc80c26080f38ddf77ea14
Vladimir Ventura <vladventura@outlook.es>
2021-06-04 02:09:59 -0400

    So I thought of a way to both avoid using useEffect and keep using the same context approach for the items: just update the items when the proxy updates

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Redirect } from "react-router-dom";
 import { Container } from "semantic-ui-react";
 import { AuthContext } from "../context/auth";
@@ -12,12 +12,11 @@ const Home = () => {
   const { user } = useContext(AuthContext);
   const { items, setItems } = useContext(ItemsContext);
 
-  const { data, loading } = useQuery(GET_ITEMS_QUERY, {});
-
-  useEffect(() => {
-    setItems((data as GetItemsQuery)?.getItems);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [(data as GetItemsQuery)?.getItems]);
+  const { data, loading } = useQuery(GET_ITEMS_QUERY, {
+    onCompleted: () => {
+      setItems((data as GetItemsQuery)?.getItems);
+    },
+  });
 
   const redirect = <Redirect to="/login" />;
   const home = (
